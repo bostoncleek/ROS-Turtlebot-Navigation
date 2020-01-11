5863fa43b2502452301ec24926481d7b30fd841b
bostoncleek <bocl8874@colorado.edu>
2020-01-11 17:14:21 -0600

    re-writes test output after each time program is run

--- rigid2d/test_compile.bash
+++ rigid2d/test_compile.bash
@@ -4,12 +4,12 @@
 g++ -Wall -Wextra -g -std=c++17 -o rigid2d_test main.cpp rigid2d.cpp
 
 # clear contents of code output file
-> output.txt
+#> output.txt
 
 # run code with test inputs adn store results in output.txt
-./rigid2d_test < test1_input.txt >> output.txt
+./rigid2d_test < test1_input.txt > output.txt
 
-# test if results match 
+# test if results match
 cmp -s output.txt test1_answer.txt
 status=$?
 if [[ $status = 0 ]]; then
