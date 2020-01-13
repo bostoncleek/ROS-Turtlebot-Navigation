872a4f92025ba408658773d0b06899478f5b9191
bostoncleek <bocl8874@colorado.edu>
2020-01-13 13:02:13 -0600

    removed comments for testing

--- rigid2d/test_compile.bash
+++ rigid2d/test_compile.bash
@@ -3,9 +3,6 @@
 # compile the program
 g++ -Wall -Wextra -g -std=c++17 -o rigid2d_test main.cpp rigid2d.cpp
 
-# clear contents of code output file
-#> output.txt
-
 # run code with test inputs adn store results in output.txt
 ./rigid2d_test < test1_input.txt > output.txt
 
