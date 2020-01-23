0197979ff95ca94e913735335784b4c1a5619543
bostoncleek <bocl8874@colorado.edu>
2020-01-22 21:14:18 -0600

    newly created waypoints class

---
+++ rigid2d/include/rigid2d/waypoints.hpp
@@ -0,0 +1,32 @@
+#ifndef WAYPOINTS_INCLUDE_GUARD_HPP
+#define WAYPOINTS_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Library for two-dimensional rigid body transformations.
+
+#include <cmath>
+#include <iosfwd>
+
+
+
+namespace rigid2d
+{
+  class Waypoints
+  {
+  public:
+    Waypoints();
+
+
+
+  private:
+
+
+  };
+
+
+
+
+
+
+} // end namespace
+
+#endif
---
+++ rigid2d/src/rigid2d/waypoints.cpp
@@ -0,0 +1,20 @@
+/// \file
+/// \brief implementation of waypoint methods in rigid2d namespace
+
+#include <iostream>
+#include "rigid2d/waypoints.hpp"
+
+
+
+namespace rigid2d
+{
+
+
+
+
+
+
+
+
+
+} // end namespace
