018299bd08fd6eda2b7fd3eaec8a32b7ad6fde10
bostoncleek <bocl8874@colorado.edu>
2020-01-25 09:23:17 -0600

    waypoints class complete.. now needs testing

--- rigid2d/include/rigid2d/waypoints.hpp
+++ rigid2d/include/rigid2d/waypoints.hpp
@@ -5,7 +5,10 @@
 
 #include <cmath>
 #include <iosfwd>
+#include <vector>
 
+#include "rigid2d/rigid2d.hpp"
+#include "rigid2d/diff_drive.hpp"
 
 
 namespace rigid2d
@@ -13,13 +16,46 @@ namespace rigid2d
   class Waypoints
   {
   public:
-    Waypoints();
+    /// \brief create a waypoints object for developing contols
+    /// \param pts - list of waypoints to travel to
+    /// \param rot_vel - roation velocity of turtle
+    /// \param trans_vel - translation velocity of turtle
+    // TODO: pass in wheel base and radius to DiffDrive
+    Waypoints(std::vector<Vector2D> way_pts, double rot_vel, double trans_vel);
+
+    /// \brief compose velocity for current waypoint
+    /// \param pose - pose of robot in world coordinates
+    /// \return cmd twist in the body frame
+    Twist2D nextWaypoint(Pose pose);
+
+    /// \brief increments the waypoint index
+    void incrementWaypoint();
+
+    /// \brief increments waypoint index if waypoint is reached 
+    /// \param pose - pose of robot in world coordinates
+    void waypointReached(Pose pose);
+
+    /// \brief compose distance from robot to waypoint
+    /// \param pose - pose of robot in world coordinates
+    double waypointDistance(Pose pose);
+
+    /// \brief compose difference in heading of robot to waypoint
+    /// \param pose - pose of robot in world coordinates
+    /// \return difference in heading
+    double waypointHeading(Pose pose);
+
+    /// \brief compose control solution for driving between
+    ///         the waypoints
+    void trajectory();
 
 
 
   private:
-
-
+    DiffDrive drive;                // diff_drive object
+    std::vector<Vector2D> pts;      // store waypoints
+    int idx;                        // index of waypoint currently headed towards
+    double htol, ptol;              // tolerances for heading and position
+    double rot_vel, trans_vel;      // rotation/translatin velocities
   };
 
 
--- rigid2d/src/rigid2d/waypoints.cpp
+++ rigid2d/src/rigid2d/waypoints.cpp
@@ -9,8 +9,115 @@
 namespace rigid2d
 {
 
+Waypoints::Waypoints(std::vector<Vector2D>  way_pts, double rot_vel, double trans_vel)
+                      : drive()
+{
+  // store waypoints
+  this->pts = way_pts;
+
+  // headed towards 1
+  idx = 1;
+
+  // robot placed at 0
+  Pose ps;
+  ps.theta = 0;
+  ps.x = pts.at(0).x;
+  ps.y = pts.at(0).y;
+
+  drive.reset(ps);
+
+  // tolerances
+  htol = 0.175;
+  ptol = 0.1;
+
+  // velocities
+  this->rot_vel = rot_vel;
+  this->trans_vel = trans_vel;
+}
 
 
+Twist2D Waypoints::nextWaypoint(Pose pose)
+{
+  Twist2D cmd;
+
+  double h_err = waypointHeading(pose);
+  h_err = normalize_angle_2PI(h_err);
+
+  // turn
+  if (std::fabs(h_err) > htol)
+  {
+    if (h_err <= PI and h_err >= 0)
+      cmd.w = rot_vel;
+    else
+      cmd.w = -rot_vel;
+
+    cmd.vx = 0;
+    cmd.vy = 0;
+
+    return cmd;
+  }
+
+  // go straight
+  cmd.w = 0;
+  cmd.vx = trans_vel;
+  cmd.vy = 0;
+
+  return cmd;
+}
+
+
+void Waypoints::waypointReached(Pose pose)
+{
+  if (waypointDistance(pose) < ptol)
+  {
+    std::cout << "Reached waypoint: " << idx << std::endl;
+    incrementWaypoint();
+  }
+}
+
+
+void Waypoints::incrementWaypoint()
+{
+  idx++;
+  // last waypoint reached head back to start
+  if (idx % pts.size() == 0)
+    idx = 0;
+}
+
+
+double Waypoints::waypointDistance(Pose pose)
+{
+  double xpt = pts.at(idx).x;
+  double ypt = pts.at(idx).y;
+
+  return std::sqrt(std::pow(xpt - pose.x, 2) + std::pow(ypt - pose.y, 2));
+}
+
+
+double Waypoints::waypointHeading(Pose pose)
+{
+  double xpt = pts.at(idx).x;
+  double ypt = pts.at(idx).y;
+  double bearing = std::atan2(ypt - pose.y, xpt - pose.x);
+
+  return bearing - pose.theta;
+}
+
+
+void Waypoints::trajectory()
+{
+  // current pose
+  Pose pose = drive.pose();
+
+  // current goal reached
+  waypointReached(pose);
+
+  // twist to follow
+  Twist2D cmd = nextWaypoint(pose);
+
+  // propogate kinematics
+  drive.feedforward(cmd);
+}
 
 
 
@@ -18,3 +125,20 @@ namespace rigid2d
 
 
 } // end namespace
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
