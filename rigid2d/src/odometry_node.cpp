6947c7e5de9865623d0cbbd2ae203b6c590b1e18
bostoncleek <bocl8874@colorado.edu>
2020-01-25 21:23:05 -0600

    odometer works

--- rigid2d/src/odometry_node.cpp
+++ rigid2d/src/odometry_node.cpp
@@ -5,12 +5,10 @@
 /// \date 1/23/20
 ///
 /// PUBLISHES:
-/// joint_states (sensor_msgs/JointState): angular wheel positions
+///   odom (nav_msgs/Odometry): Pose of robot in odom frame and twist in body frame
 ///
 /// SUBSCRIBES:
-/// odom (nav_msgs/Odometry): Pose of robot in odom frame and twist in body frame
-
-
+///   joint_states (sensor_msgs/JointState): angular wheel positions
 
 
 #include <ros/ros.h>
@@ -30,6 +28,7 @@
 // global variables
 std::string left_wheel_joint, right_wheel_joint;    // joint names
 double left, right;                                // wheel angular positions
+bool message;
 
 /// \brief updates the wheel encoder angles
 /// \param msg - contains the encoder readings and joint names
@@ -45,20 +44,17 @@ void jointStatesCallback(const sensor_msgs::JointState::ConstPtr &msg)
   iter = std::find(names.begin(), names.end(), right_wheel_joint);
   right_idx = std::distance(names.begin(), iter);
 
-  // ROS_INFO("%d", left_idx);
-  // ROS_INFO("%d", right_idx);
+  // ROS_INFO("Left wheel %f", left);
+  // ROS_INFO("Right wheel %f", right);
 
   left = msg->position.at(left_idx);
   right = msg->position.at(right_idx);
+
+  message = true;
 }
 
 // TODO: change to tf2
 
-///////////////////////////////////////////////////////////////
-// TODO: I think encoders are wrapped -pi to pi
-//        might need to update angle wrapping in diff drive
-
-///////////////////////////////////////////////////////////////
 
 
 int main(int argc, char** argv)
@@ -91,15 +87,22 @@ int main(int argc, char** argv)
   ROS_INFO("Successfully launched odometer node.");
 
 
+  // check if message has been recieved
+  message = false;
+
   rigid2d::Pose pose;
   pose.theta = 0;
   pose.x = 0;
   pose.y = 0;
 
+
   // diff drive model
   rigid2d::DiffDrive drive(pose, wheel_base, wheel_radius);
 
   // timing
+  // int frequency = 60;
+  // ros::Rate loop_rate(frequency);
+
   ros::Time current_time, last_time;
   current_time = ros::Time::now();
   last_time = ros::Time::now();
@@ -110,63 +113,71 @@ int main(int argc, char** argv)
     ros::spinOnce();
     current_time = ros::Time::now();
 
-    // ROS_INFO("Left wheel %f", left);
-    // ROS_INFO("Right wheel %f", right);
-
-    // update odom
-    drive.updateOdometry(left, right);
-
-    // pose
-    pose = drive.pose();
-    // std::cout << "------------------" << std::endl;
-    // std::cout << pose.theta << " " << pose.x << " " << pose.y << std::endl;
-    // std::cout << "------------------" << std::endl;
-
-    // body twist
-    rigid2d::WheelVelocities vel = drive.wheelVelocities();
-    rigid2d::Twist2D vb = drive.wheelsToTwist(vel);
-
-    // quaternion from yaw
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pose.theta);
-    // std::cout << odom_quat << "\n";
+    if (message)
+    {
+      ROS_INFO("Left wheel %f", left);
+      ROS_INFO("Right wheel %f", right);
+      // ROS_INFO("Right wheel %f", right);
 
 
-    // broadcast transform between odom and body
-    geometry_msgs::TransformStamped odom_tf;
-    odom_tf.header.stamp = current_time;
-    odom_tf.header.frame_id = odom_frame_id;
-    odom_tf.child_frame_id = body_frame_id;
+      // update odom
+      drive.updateOdometry(left, right);
 
-    odom_tf.transform.translation.x = pose.x;
-    odom_tf.transform.translation.y = pose.y;
-    odom_tf.transform.translation.z = 0.0;
-    odom_tf.transform.rotation = odom_quat;
+      // pose relative to odom frame
+      pose = drive.pose();
+      // std::cout << "------------------" << std::endl;
+      // std::cout << "odometer" << std::endl;
+      // std::cout << pose.theta << " " << pose.x << " " << pose.y << std::endl;
+      // std::cout << "------------------" << std::endl;
 
-    odom_broadcaster.sendTransform(odom_tf);
+      // body twist
+      rigid2d::WheelVelocities vel = drive.wheelVelocities();
+      rigid2d::Twist2D vb = drive.wheelsToTwist(vel);
 
-
-    // publish odom message over ros
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = odom_frame_id;
-    odom.child_frame_id = body_frame_id;
+      // quaternion from yaw
+      geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pose.theta);
+      // std::cout << odom_quat << "\n";
 
 
-    // pose in odom frame
-    odom.pose.pose.position.x = pose.x;
-    odom.pose.pose.position.y = pose.y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
+      // broadcast transform between odom and body
+      geometry_msgs::TransformStamped odom_tf;
+      odom_tf.header.stamp = current_time;
+      odom_tf.header.frame_id = odom_frame_id;
+      odom_tf.child_frame_id = body_frame_id;
 
-    // velocity in body frame
-    odom.twist.twist.linear.x = vb.vx;
-    odom.twist.twist.linear.y = vb.vy; // should always be 0
-    odom.twist.twist.angular.z = vb.w;
+      odom_tf.transform.translation.x = pose.x;
+      odom_tf.transform.translation.y = pose.y;
+      odom_tf.transform.translation.z = 0.0;
+      odom_tf.transform.rotation = odom_quat;
 
-    odom_pub.publish(odom);
+      odom_broadcaster.sendTransform(odom_tf);
 
+
+      // publish odom message over ros
+      nav_msgs::Odometry odom;
+      odom.header.stamp = current_time;
+      odom.header.frame_id = odom_frame_id;
+      odom.child_frame_id = body_frame_id;
+
+
+      // pose in odom frame
+      odom.pose.pose.position.x = pose.x;
+      odom.pose.pose.position.y = pose.y;
+      odom.pose.pose.position.z = 0.0;
+      odom.pose.pose.orientation = odom_quat;
+
+      // velocity in body frame
+      odom.twist.twist.linear.x = vb.vx;
+      odom.twist.twist.linear.y = vb.vy; // should always be 0
+      odom.twist.twist.angular.z = vb.w;
+
+      odom_pub.publish(odom);
+
+      message = false;
+    }
 
     last_time = current_time;
+    // loop_rate.sleep();
   }
 
   return 0;
