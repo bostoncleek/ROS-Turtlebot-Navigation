72e37836a541c06b639534346bacaee626375d3e
bostoncleek <bocl8874@colorado.edu>
2020-02-06 18:14:37 -0600

    new package

---
+++ nuturtle_robot/src/turtle_interface_node.cpp
@@ -0,0 +1,238 @@
+/// \file
+/// \brief
+///
+/// \author Boston Cleek
+/// \date 2/5/20
+///
+/// PUBLISHES:
+///
+///
+/// SUBSCRIBES:
+///
+
+
+
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/JointState.h>
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+
+#include <rigid2d/rigid2d.hpp>
+#include <rigid2d/diff_drive.hpp>
+#include <nuturtlebot/WheelCommands.h>
+#include <nuturtlebot/SensorData.h>
+
+
+
+// global variables
+static rigid2d::Twist2D cmd;                                // body cmd_vel
+static bool twist_message;                                  // twist callback flag
+
+static double left, right;                                 //  encoder ticks
+static bool sensor_message;                                // sensor callback flag
+
+
+
+/// \brief updates the body twist of the robot
+/// \param msg - contains the velocities of the body frame
+void twistCallBack(const geometry_msgs::Twist::ConstPtr &msg)
+{
+  cmd.w = msg->angular.z;
+  cmd.vx = msg->linear.x;
+  cmd.vy = 0.0;
+
+
+  // ROS_INFO("twistCallback");
+  twist_message = true;
+}
+
+
+
+/// \brief updates wheel encoder readings
+/// \param msg - contains sensor information
+void sensorCallBack(const nuturtlebot::SensorData::ConstPtr &msg)
+{
+  left = msg->left_encoder;
+  right = msg->right_encoder;
+
+  sensor_message = true;
+}
+
+
+
+int main(int argc, char** argv)
+{
+
+  ros::init(argc, argv, "turtle_interface");
+  ros::NodeHandle node_handle;
+
+  ros::Subscriber vel_sub = node_handle.subscribe("/cmd_vel", 1, twistCallBack);
+  ros::Subscriber sensor_sub = node_handle.subscribe("/sensor_data", 1, sensorCallBack);
+
+  ros::Publisher wheel_pub = node_handle.advertise<nuturtlebot::WheelCommands>("/wheel_cmd", 1);
+  ros::Publisher joint_pub = node_handle.advertise<sensor_msgs::JointState>("/joint_states", 1);
+
+
+  double max_rot = 0.0, max_trans = 0.0;
+  double wheel_base = 0.0, wheel_radius = 0.0;
+  double max_rot_motor = 0.0, encoder_ticks_per_rev = 0.0;
+
+  std::string left_wheel_joint, right_wheel_joint;
+
+  node_handle.getParam("max_rot", max_rot);
+  node_handle.getParam("max_trans", max_trans);
+  node_handle.getParam("max_rot_motor", max_rot_motor);
+  node_handle.getParam("encoder_ticks_per_rev", encoder_ticks_per_rev);
+
+
+  node_handle.getParam("/wheel_base", wheel_base);
+  node_handle.getParam("/wheel_radius", wheel_radius);
+
+
+  node_handle.getParam("/left_wheel_joint", left_wheel_joint);
+  node_handle.getParam("/right_wheel_joint", right_wheel_joint);
+
+
+  // init pose of robot (0,0,0)
+  rigid2d::Pose ps;
+
+  // model the robot
+  rigid2d::DiffDrive drive(ps, wheel_base, wheel_radius);
+
+
+  // timing
+   int frequency = 60;
+   ros::Rate loop_rate(frequency);
+
+   ros::Time current_time, last_time;
+   current_time = ros::Time::now();
+   last_time = ros::Time::now();
+
+
+   // TODO: math_utils::clamp
+
+
+  while(node_handle.ok())
+  {
+    ros::spinOnce();
+    current_time = ros::Time::now();
+
+
+    if (twist_message)
+    {
+      // cap velocities
+      if (cmd.w > max_rot)
+      {
+        cmd.w = max_rot;
+      }
+
+      if (cmd.vx > max_trans)
+      {
+        cmd.vx = max_trans;
+      }
+
+
+      // compose wheel velocities from twist
+      rigid2d::WheelVelocities wheel_vel = drive.twistToWheels(cmd);
+
+      // cap wheel velocities
+      if (wheel_vel.ul > max_rot_motor)
+      {
+        wheel_vel.ul = max_rot_motor;
+      }
+
+      // cap wheel velocities
+      if (wheel_vel.ur > max_rot_motor)
+      {
+        wheel_vel.ur = max_rot_motor;
+      }
+
+
+      // scale wheel velocities -44 -> 44
+      nuturtlebot::WheelCommands wheel_command;
+      wheel_command.left_velocity = std::round((44.0 / max_rot_motor) * wheel_vel.ul);
+      wheel_command.right_velocity = std::round((44.0 / max_rot_motor) * wheel_vel.ur);
+
+
+      // ROS_INFO("left wheel cmd %f", wheel_vel.ul);
+      // ROS_INFO("right wheel cmd %f", wheel_vel.ur);
+
+      // ROS_INFO("left wheel cmd %d", wheel_command.left_velocity);
+      // ROS_INFO("right wheel cmd %d", wheel_command.right_velocity);
+
+      twist_message = false;
+
+      wheel_pub.publish(wheel_command);
+    }
+
+
+    if (sensor_message)
+    {
+
+      // convert endcoder ticks to wheel angles
+      double left_angle = (rigid2d::PI / encoder_ticks_per_rev) * left;
+      double right_angle = (rigid2d::PI / encoder_ticks_per_rev) * right;
+
+
+      // update odometry to get wheel velocities
+      rigid2d::WheelVelocities wheel_vel =  drive.updateOdometry(left_angle, right_angle);
+
+
+      sensor_msgs::JointState joint_state;
+      joint_state.header.stamp = current_time;
+
+      joint_state.name.push_back(left_wheel_joint);
+      joint_state.name.push_back(right_wheel_joint);
+
+      joint_state.position.push_back(left_angle);
+      joint_state.position.push_back(right_angle);
+
+      joint_state.velocity.push_back(wheel_vel.ul);
+      joint_state.velocity.push_back(wheel_vel.ur);
+
+
+      ROS_INFO("left wheel pos %f", left_angle);
+      ROS_INFO("right wheel pos %f", right_angle);
+
+      ROS_INFO("left wheel vel %f", wheel_vel.ul);
+      ROS_INFO("right wheel vel %f", wheel_vel.ur);
+
+
+      sensor_message = false;
+
+      joint_pub.publish(joint_state);
+    }
+
+
+    last_time = current_time;
+    loop_rate.sleep();
+  }
+
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end file
---
+++ nuturtle_robot/test/turtle_interface_test_node.cpp
@@ -0,0 +1,245 @@
+/// \file
+/// \brief
+///
+/// \author Boston Cleek
+/// \date 2/5/20
+///
+/// PUBLISHES:
+///
+///
+/// SUBSCRIBES:
+///
+
+
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/JointState.h>
+
+#include <string>
+#include <vector>
+
+#include <nuturtlebot/WheelCommands.h>
+#include <nuturtlebot/SensorData.h>
+
+
+
+/// \brief Stores wheel joint state
+struct WheelAxel
+{
+  std::string name;
+  double position = 0.0;
+  double velocity = 0.0;
+};
+
+
+/// \brief Helps test the turtlebot wheel speed and sensor msgs
+struct TurtlebotHelper
+{
+  TurtlebotHelper() : left_vel(0), right_vel(0),
+                      wheelVelFlag(false), jointStatesFlag(false) {};
+
+  /// \brief Callback for wheel speeds
+  /// \param msg - wheel speed message
+  void wheelCallBack(const nuturtlebot::WheelCommands::ConstPtr &msg)
+  {
+    left_vel = msg->left_velocity;
+    right_vel = msg->right_velocity;
+    wheelVelFlag = true;
+  }
+
+  /// \brief Callback for joint states
+  /// \param msg - joint name, position, and angular velocity
+  void jointCallBack(const sensor_msgs::JointState::ConstPtr &msg)
+  {
+    // first index if the left wheel
+    left_joint.name = msg->name.at(0);
+    left_joint.position = msg->position.at(0);
+    left_joint.velocity = msg->velocity.at(0);
+
+    right_joint.name = msg->name.at(1);
+    right_joint.position = msg->position.at(1);
+    right_joint.velocity = msg->velocity.at(1);
+
+    jointStatesFlag = true;
+  }
+
+
+  int left_vel;             // left wheel velocity
+  int right_vel;            // right wheel velocity
+  bool wheelVelFlag;        // flag for message received
+
+  WheelAxel left_joint;     // left wheel joint
+  WheelAxel right_joint;    // right wheel joint
+  bool jointStatesFlag;     // flag for joint states message
+};
+
+
+
+
+
+/// \brief Tests the wheel speed drive robot translation
+TEST(TurtleInterface, TwistNoRotation)
+{
+  ros::NodeHandle node_handle;
+
+  TurtlebotHelper turthelper;
+
+
+  ROS_INFO("work?");
+
+
+  // latched publisher
+  ros::Publisher cmd_pub = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
+  ros::Subscriber wheel_sub = node_handle.subscribe("/wheel_cmd", 1, &TurtlebotHelper::wheelCallBack, &turthelper);
+
+  geometry_msgs::Twist cmd;
+  cmd.linear.x = 0.1;
+  cmd.linear.y = 0;
+  cmd.linear.z = 0;
+  cmd.angular.x = 0;
+  cmd.angular.y = 0;
+  cmd.angular.z = 0;
+
+
+  while(!turthelper.wheelVelFlag)
+  {
+    ros::spinOnce();
+    cmd_pub.publish(cmd);
+  }
+
+  ASSERT_EQ(turthelper.left_vel, 21);
+  ASSERT_EQ(turthelper.right_vel, 21);
+}
+
+
+
+/// \brief Tests the wheel speed drive robot rotation
+TEST(TurtleInterface, TwistNoTranslation)
+{
+  ros::NodeHandle node_handle;
+
+  TurtlebotHelper turthelper;
+
+
+  // latched publisher
+  ros::Publisher cmd_pub = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
+  ros::Subscriber wheel_sub = node_handle.subscribe("/wheel_cmd", 1, &TurtlebotHelper::wheelCallBack, &turthelper);
+
+  geometry_msgs::Twist cmd;
+  cmd.linear.x = 0;
+  cmd.linear.y = 0;
+  cmd.linear.z = 0;
+  cmd.angular.x = 0;
+  cmd.angular.y = 0;
+  cmd.angular.z = 1;
+
+
+  while(!turthelper.wheelVelFlag)
+  {
+    ros::spinOnce();
+    cmd_pub.publish(cmd);
+  }
+
+  ASSERT_EQ(turthelper.left_vel, -17);
+  ASSERT_EQ(turthelper.right_vel, 17);
+}
+
+
+/// \brief Tests translation and rotation wheel speeds
+TEST(TurtleInterface, TwistTransRot)
+{
+  ros::NodeHandle node_handle;
+
+  TurtlebotHelper turthelper;
+
+
+  // latched publisher
+  ros::Publisher cmd_pub = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
+  ros::Subscriber wheel_sub = node_handle.subscribe("/wheel_cmd", 1, &TurtlebotHelper::wheelCallBack, &turthelper);
+
+  geometry_msgs::Twist cmd;
+  cmd.linear.x = 0.01;
+  cmd.linear.y = 0;
+  cmd.linear.z = 0;
+  cmd.angular.x = 0;
+  cmd.angular.y = 0;
+  cmd.angular.z = 1;
+
+
+  while(!turthelper.wheelVelFlag)
+  {
+    ros::spinOnce();
+    cmd_pub.publish(cmd);
+  }
+
+  ASSERT_EQ(turthelper.left_vel, -15);
+  ASSERT_EQ(turthelper.right_vel, 19);
+}
+
+
+
+/// \brief Tests translation and rotation wheel speeds
+TEST(TurtleInterface, Sensort2JointState)
+{
+  ros::NodeHandle node_handle;
+
+  TurtlebotHelper turthelper;
+
+
+  // latched publisher
+  ros::Publisher sensor_pub = node_handle.advertise<nuturtlebot::SensorData>("/sensor_data", 1, true);
+  ros::Subscriber wheel_sub = node_handle.subscribe("/joint_states", 1, &TurtlebotHelper::jointCallBack, &turthelper);
+
+
+  nuturtlebot::SensorData sensor_data;
+  sensor_data.left_encoder = 100;
+  sensor_data.right_encoder = 100;
+
+
+  while(!turthelper.jointStatesFlag)
+  {
+    ros::spinOnce();
+    sensor_pub.publish(sensor_data);
+  }
+
+  ASSERT_NEAR(turthelper.left_joint.position, 0.076699, 1e-3);
+  ASSERT_NEAR(turthelper.right_joint.position, 0.076699,1e-3);
+
+  ASSERT_NEAR(turthelper.left_joint.velocity, 0.076699, 1e-3);
+  ASSERT_NEAR(turthelper.right_joint.velocity, 0.076699,1e-3);
+}
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "turtle_interface_test");
+  return RUN_ALL_TESTS();
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end file
