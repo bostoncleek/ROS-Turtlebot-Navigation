0d0a8981b8f3c37c77d8e371790f34e6f0759651
bostoncleek <bocl8874@colorado.edu>
2020-02-14 16:18:36 -0600

    feedback for waypoints

--- rigid2d/src/rigid2d/waypoints.cpp
+++ rigid2d/src/rigid2d/waypoints.cpp
@@ -2,6 +2,7 @@
 /// \brief Library for generating control (body twist) between waypoints
 
 #include <iostream>
+#include <algorithm>
 #include "rigid2d/waypoints.hpp"
 
 
@@ -10,22 +11,25 @@ namespace rigid2d
 {
 
 Waypoints::Waypoints(const std::vector<Vector2D> &way_pts, double rot_vel, double trans_vel)
+        : pts(way_pts),
+          idx(0),
+          ctr(0),
+          htol(0.005),
+          ptol(0.1),
+          rot_vel(rot_vel),
+          trans_vel(trans_vel),
+          k_rot(0.0),
+          k_trans(0.0),
+          cycle_complete(false)
 {
-  // store waypoints
-  this->pts = way_pts;
-
-  // assume NOT starting at 0
-  // head to 0
-  idx = 0;
-
-  // tolerances
-  htol = 0.005;   // heading tolerance
-  ptol = 0.1;     // distance to goal tolerance
-
-  // velocities
-  this->rot_vel = rot_vel;
-  this->trans_vel = trans_vel;
 }
+
+
+void Waypoints::setGain(double k_rot)
+{
+  this->k_rot = k_rot;
+}
+
 
 
 Twist2D Waypoints::nextWaypoint(Pose pose)
@@ -63,6 +67,48 @@ Twist2D Waypoints::nextWaypoint(Pose pose)
 }
 
 
+Twist2D Waypoints::nextWaypointClosedLoop(Pose pose)
+{
+  Twist2D cmd;
+
+  // current goal reached
+  waypointReached(pose);
+
+  // stop after one cycle
+  if (cycle_complete)
+  {
+    return cmd;
+  }
+
+
+  const auto h_err = normalize_angle_PI(waypointHeading(pose));
+  const auto p_err = waypointDistance(pose);
+
+
+  // go straight
+  // cant go in reverse
+  if (std::fabs(h_err) < htol)
+  {
+    cmd.vx = trans_vel;
+    cmd.w = 0;
+    cmd.vy = 0;
+
+    return cmd;
+  }
+
+
+  // turn
+  cmd.w = k_rot * h_err;
+  cmd.w = std::clamp(cmd.w, -rot_vel, rot_vel);
+
+  cmd.vx = 0;
+  cmd.vy = 0;
+
+  return cmd;
+}
+
+
+
 void Waypoints::waypointReached(Pose pose)
 {
   if (waypointDistance(pose) < ptol)
@@ -76,10 +122,20 @@ void Waypoints::waypointReached(Pose pose)
 void Waypoints::incrementWaypoint()
 {
   idx++;
+  ctr++;
 
   // last waypoint reached head back to start
   if (idx % pts.size() == 0)
+  {
     idx = 0;
+  }
+
+  if (ctr == static_cast<int> (pts.size()+1))
+  {
+    cycle_complete = true;
+    std::cout << "Once Cyle Complete" << std::endl;
+    return;
+  }
 
   std::cout << "Headed to waypoint: " << idx << " at ["
   << pts.at(idx).x << " " << pts.at(idx).y << "]" << std::endl;
