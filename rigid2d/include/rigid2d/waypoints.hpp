620520b2bd8d17af85e9ace23724ac230e2b1836
bostoncleek <bocl8874@colorado.edu>
2020-02-14 16:18:24 -0600

    feedback for waypoints

--- rigid2d/include/rigid2d/waypoints.hpp
+++ rigid2d/include/rigid2d/waypoints.hpp
@@ -22,12 +22,22 @@ namespace rigid2d
     /// \param trans_vel - translation velocity of turtle
     Waypoints(const std::vector<Vector2D> &way_pts, double rot_vel, double trans_vel);
 
+    /// \brief Sets the control gain
+    /// \param k_rot - gain for rotation
+    /// \param k_trans - gain for translation
+    void setGain(double k_rot);
+
     /// \brief compose velocity for current waypoint
     /// \param pose - pose of robot in world coordinates
     /// \return cmd twist in the body frame
     Twist2D nextWaypoint(Pose pose);
 
+    /// \brief compose velocity for current waypoint using closed loop feedback
+    /// \param pose - pose of robot in world coordinates
+    /// \return cmd twist in the body frame
+    Twist2D nextWaypointClosedLoop(Pose pose);
 
+  private:
     /// \brief increments the waypoint index
     void incrementWaypoint();
 
@@ -44,13 +54,15 @@ namespace rigid2d
     /// \return difference in heading
     double waypointHeading(Pose pose) const;
 
-
-  private:
-    // DiffDrive drive;                // diff_drive object
+    // DiffDrive drive;             // diff_drive object
     std::vector<Vector2D> pts;      // store waypoints
     int idx;                        // index of waypoint currently headed towards
+    int ctr;                        // count total waypoints visited
     double htol, ptol;              // tolerances for heading and position
     double rot_vel, trans_vel;      // rotation/translatin velocities
+    double k_rot;                   // rotation gain
+    double k_trans;                 // translation gain
+    bool cycle_complete;            // one cycle to all waypoints complete
   };
 
 
