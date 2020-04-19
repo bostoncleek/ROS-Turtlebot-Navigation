168ca4c19a8ce05f62ed2bf4d1aad311e2a568b5
bostoncleek <bocl8874@colorado.edu>
2020-04-18 21:37:57 -0500

    import distance functions from planner utils

--- planner/include/planner/road_map.hpp
+++ planner/include/planner/road_map.hpp
@@ -8,20 +8,14 @@
 #include <iosfwd>
 #include <unordered_set>
 
-#include <rigid2d/utilities.hpp>
-#include <rigid2d/rigid2d.hpp>
-
-
+#include "planner/planner_utilities.hpp"
 
 namespace planner
 {
+  
   using rigid2d::sampleUniformDistribution;
   using rigid2d::Vector2D;
   using rigid2d::euclideanDistance;
-
-
-  typedef std::vector<Vector2D> polygon;      // convex polygon, vertices counter-clockwise
-  typedef std::vector<polygon> obstacle_map;  // constain all polygons in Cspace
 
 
   /// \brief Egde connecting two nodes
@@ -51,56 +45,6 @@ namespace planner
   };
 
 
-  /// \brief Useful information for determining the closest point to
-  ///        a line segment
-  struct ClosePoint
-  {
-    double t = 0.0;           // if t [0, 1] then the min distance is on the line segment
-    double sign_d = 0.0;      // signed distance from line to p, d > 0 if on left side in direction p1 => p2
-    Vector2D p;               // closest point to line
-    bool on_seg = false;      // true if min distance to p is on the segment
-  };
-
-
-  /// \brief Compose shortest distance from linesegment to a point
-  ///        p1 and p2 are the bounds of the line segment
-  ///        and p3 is the point.
-  ///        Checks if the min distance is on the line segment
-  /// \param p1 - first bound of line segment
-  /// \param p1 - second bound of line segment
-  /// \param p3 - point to compose distance to
-  /// distance[out] - min distance between line and a point
-  /// \returns - true if the closest distance lies on the line segment
-  bool minDistLineSegPt(double &distance,
-                        const Vector2D &p1,
-                        const Vector2D &p2,
-                        const Vector2D &p3);
-
-  /// \brief Compose shortest distance from linesegment to a point
-  ///        p1 and p2 are the bounds of the line segment
-  ///        and p3 is the point.
-  ///        Does not check if the min distance is on the line segment
-  /// \param p1 - first bound of line segment
-  /// \param p1 - second bound of line segment
-  /// \param p3 - point to compose distance to
-  /// \returns - min distance between line and a point
-  double minDistLineSegPt(const Vector2D &p1,
-                          const Vector2D &p2,
-                          const Vector2D &p3);
-
-  /// \brief Compose shortest distance from linesegment to a point
-  ///        p1 and p2 are the bounds of the line segment
-  ///        and p3 is the point.
-  ///        Checks if the min distance is on the line segment
-  /// \param p1 - first bound of line segment
-  /// \param p1 - second bound of line segment
-  /// \param p3 - point to compose distance to
-  /// \returns - data regarding the state of the min distance
-  ///            from a point to the line segment
-  ClosePoint signMinDist2Line(const Vector2D &p1,
-                              const Vector2D &p2,
-                              const Vector2D &p3);
-
   /// \brief Check whether line segment intersects a polygon
   /// \param poly - plygon to examine
   /// \param p1 - first bound of line segment
@@ -109,7 +53,6 @@ namespace planner
   bool lnSegIntersectPolygon(const polygon &poly,
                              const Vector2D &p1,
                              const Vector2D &p2);
-
 
    /// \brief Compose graph for global planning
   class RoadMap
--- planner/src/planner/road_map.cpp
+++ planner/src/planner/road_map.cpp
@@ -12,127 +12,6 @@ namespace planner
 {
 
 
-bool minDistLineSegPt(double &distance,
-                      const Vector2D &p1,
-                      const Vector2D &p2,
-                      const Vector2D &p3)
-{
-  // source http://paulbourke.net/geometry/pointlineplane/
-
-  const auto dx = p2.x - p1.x;
-  const auto dy = p2.y - p1.y;
-
-  const auto num = (p3.x - p1.x)*dx + (p3.y - p1.y)*dy;
-  const auto denom = dx*dx + dy*dy;
-  const auto u = num / denom;
-
-  // std::cout << u << std::endl;
-
-  // location of point (P) on line (not necessarily on the line segment)
-  const auto px = p1.x + u * dx;
-  const auto py = p1.y + u * dy;
-
-  // min distance
-  distance = euclideanDistance(px, py, p3.x, p3.y);
-
-  if (rigid2d::almost_equal(u, 0.0) or rigid2d::almost_equal(u, 1.0))
-  {
-    return true;
-  }
-
-  else if (u > 0.0 and u < 1.0)
-  {
-    return true;
-  }
-
-  // return (u >= 0.0 and u <= 1.0) ? true : false;
-  return false;
-}
-
-
-double minDistLineSegPt(const Vector2D &p1,
-                        const Vector2D &p2,
-                        const Vector2D &p3)
-{
-
-  // vector from p1 to p2 (v)
-  const auto vx = p2.x - p1.x;
-  const auto vy = p2.y - p1.y;
-
-  // leftward normal vector (u)
-  const auto ux = -vy;
-  const auto uy = vx;
-
-  // magnitude of u
-  const auto unorm = std::sqrt(ux*ux + uy*uy);
-
-  // leftward normal vector
-  const auto nx = ux / unorm;
-  const auto ny = uy / unorm;
-
-  // vector from p1 to p3
-  const auto dx = p3.x - p1.x;
-  const auto dy = p3.y - p1.y;
-
-  // signed distance is the dot product (d and n)
-  return dx*nx + dy*ny;
-}
-
-
-ClosePoint signMinDist2Line(const Vector2D &p1,
-                            const Vector2D &p2,
-                            const Vector2D &p3)
-{
-  ClosePoint clpt;
-
-  // vector from p1 to p2 (v)
-  const auto vx = p2.x - p1.x;
-  const auto vy = p2.y - p1.y;
-
-  // leftward normal vector (u)
-  const auto ux = -vy;
-  const auto uy = vx;
-
-  // magnitude of u
-  const auto unorm = std::sqrt(ux*ux + uy*uy);
-
-  // leftward normal vector (n)
-  const auto nx = ux / unorm;
-  const auto ny = uy / unorm;
-
-  // vector from p1 to p3 (d)
-  const auto dx = p3.x - p1.x;
-  const auto dy = p3.y - p1.y;
-
-  // parameteize line (t)
-  const auto num = (p3.x - p1.x)*vx + (p3.y - p1.y)*vy;
-  const auto denom = vx*vx + vy*vy;
-
-  clpt.t = num / denom;
-
-  // signed distance is the dot product (d and n)
-  clpt.sign_d = dx*nx + dy*ny;
-
-  // location of point (P) on line (not necessarily on the line segment)
-  clpt.p.x = p1.x + clpt.t * vx;
-  clpt.p.y = p1.y + clpt.t * vy;
-
-  // check if  P is on segment p1 => p2
-  if ((clpt.t > 0.0 and clpt.t < 1.0) or \
-       rigid2d::almost_equal(clpt.t, 0.0) or \
-       rigid2d::almost_equal(clpt.t, 1.0))
-  {
-    clpt.on_seg = true;
-  }
-
-  else
-  {
-    clpt.on_seg = false;
-  }
-
-  return clpt;
-}
-
 bool lnSegIntersectPolygon(const polygon &poly,
                            const Vector2D &p1,
                            const Vector2D &p2)
@@ -512,7 +391,7 @@ bool RoadMap::ptInsidePolygon(const polygon &poly, const Vector2D &q) const
             return true;
           }
         }
-      } 
+      }
 
     } // end if (right side)
   } // end loop
