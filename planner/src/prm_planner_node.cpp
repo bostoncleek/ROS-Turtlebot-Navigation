e63c5a028b64af77432b4b586089722215f65d45
bostoncleek <bocl8874@colorado.edu>
2020-04-26 12:40:23 -0500

    draws shortest path

--- planner/src/prm_planner_node.cpp
+++ planner/src/prm_planner_node.cpp
@@ -12,6 +12,7 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Point.h>
 #include <xmlrpcpp/XmlRpcValue.h>
 
@@ -21,6 +22,7 @@
 #include <iostream>
 
 #include "planner/road_map.hpp"
+#include "planner/prm_planner.hpp"
 
 using rigid2d::Vector2D;
 
@@ -29,98 +31,23 @@ static std::string frame_id;                   // frame of path marker
 
 /// \brief - Fills a marker array with deisred contents
 /// \param nodes - nodes and edges in prm
+/// \param color - color of spheres and lines
+/// \param prm - set to true if marker array is for prm
 /// nodes_array[out] - sphere markers for nodes
 /// edges_array[out] - line list markers for edges
-void fillMarkerArray(const std::vector<planner::Node> &nodes,
+void prmMarkerArray(const std::vector<planner::Node> &nodes,
                      visualization_msgs::MarkerArray &nodes_array,
-                     visualization_msgs::MarkerArray &edges_array)
-{
-
-  for(unsigned int i = 0; i < nodes.size(); i++)
-  {
-
-    // coordinates of node
-    const auto ndx = nodes.at(i).point.x;
-    const auto ndy = nodes.at(i).point.y;
-    geometry_msgs::Point nd_pt;
-    nd_pt.x = ndx;
-    nd_pt.y = ndy;
-    nd_pt.z = 0.0;
+                     visualization_msgs::MarkerArray &edges_array,
+                     std::string color);
 
 
-    // add spheres for nodes
-    nodes_array.markers[i].header.frame_id = frame_id;
-    nodes_array.markers[i].header.stamp = ros::Time::now();
-    nodes_array.markers[i].lifetime = ros::Duration();
-    nodes_array.markers[i].ns = "prm_nodes";
-    nodes_array.markers[i].id = i;
-
-    nodes_array.markers[i].type = visualization_msgs::Marker::SPHERE;
-    nodes_array.markers[i].action = visualization_msgs::Marker::ADD;
-
-    nodes_array.markers[i].pose.position.x = ndx;
-    nodes_array.markers[i].pose.position.y = ndy;
-    nodes_array.markers[i].pose.position.z = 0.0;
-
-    nodes_array.markers[i].pose.orientation.x = 0.0;
-    nodes_array.markers[i].pose.orientation.y = 0.0;
-    nodes_array.markers[i].pose.orientation.z = 0.0;
-    nodes_array.markers[i].pose.orientation.w = 1.0;
-
-    nodes_array.markers[i].scale.x = 0.04;
-    nodes_array.markers[i].scale.y = 0.04;
-    nodes_array.markers[i].scale.z = 0.04;
-
-    nodes_array.markers[i].color.r = 0.0f;
-    nodes_array.markers[i].color.g = 1.0f;
-    nodes_array.markers[i].color.b = 0.0f;
-    nodes_array.markers[i].color.a = 1.0f;
-
-
-    // add line strip for edges
-    edges_array.markers[i].header.frame_id = frame_id;
-    edges_array.markers[i].header.stamp = ros::Time::now();
-    edges_array.markers[i].lifetime = ros::Duration();
-    edges_array.markers[i].ns = "prm_edges";
-    edges_array.markers[i].id = i;
-
-    edges_array.markers[i].type = visualization_msgs::Marker::LINE_LIST;
-    edges_array.markers[i].action = visualization_msgs::Marker::ADD;
-
-    edges_array.markers[i].scale.x = 0.02;
-
-    edges_array.markers[i].pose.orientation.x = 0.0;
-    edges_array.markers[i].pose.orientation.y = 0.0;
-    edges_array.markers[i].pose.orientation.z = 0.0;
-    edges_array.markers[i].pose.orientation.w = 1.0;
-
-
-    edges_array.markers[i].color.r = 0.0f;
-    edges_array.markers[i].color.g = 1.0f;
-    edges_array.markers[i].color.b = 0.0f;
-    edges_array.markers[i].color.a = 1.0f;
-
-
-    // loop across adjacency list
-    for(unsigned int j = 0; j < nodes.at(i).edges.size(); j++)
-    {
-      const auto adj_id = nodes.at(i).edges.at(j).id;
-
-      geometry_msgs::Point adj_pt; // adjacent node point
-      adj_pt.x = nodes.at(adj_id).point.x;
-      adj_pt.y = nodes.at(adj_id).point.y;
-      adj_pt.z = 0.0;
-
-      edges_array.markers[i].points.push_back(nd_pt);
-      edges_array.markers[i].points.push_back(adj_pt);
-
-    } // end inner loop
-  } // end outer loop
-
-}
-
-
-
+/// \brief - Fills a markers with deisred contents
+/// \param path - nodes in shortest path
+/// point[out] - markers for nodes
+/// line_strip[out] - line strip markers for edges between nodes
+void pathMarkers(const std::vector<Vector2D> &path,
+                 visualization_msgs::Marker &points,
+                 visualization_msgs::Marker &line_strip);
 
 
 int main(int argc, char** argv)
@@ -129,8 +56,8 @@ int main(int argc, char** argv)
   ros::NodeHandle nh("~");
   ros::NodeHandle node_handle;
 
-  ros::Publisher node_pub = node_handle.advertise<visualization_msgs::MarkerArray>("prm_nodes", 1);
-  ros::Publisher edge_pub = node_handle.advertise<visualization_msgs::MarkerArray>("prm_edges", 1);
+  ros::Publisher prm_pub = node_handle.advertise<visualization_msgs::MarkerArray>("prm", 1);
+  ros::Publisher path_pub = node_handle.advertise<visualization_msgs::Marker>("shortest_path", 1);
 
 
   auto resolution = 0.0;                  // resolution of map coordinates
@@ -164,8 +91,8 @@ int main(int argc, char** argv)
 
   /////////////////////////////////////////////////////////////////////////////
 
+  // store all the obstacles
   planner::obstacle_map obs_map;
-
   for(auto i = 0; i < obstacles.size(); i++)
   {
     planner::polygon poly;
@@ -188,39 +115,250 @@ int main(int argc, char** argv)
                        obs_map);
 
 
-  Vector2D start(0.2, 0.2);
-  Vector2D goal(0.9, 2.6);
+  // declare the start/goal
+  Vector2D start(2.1, 0.9);
+  Vector2D goal(6.0, 13.5);
 
+
+  // construct the prm
   prm.constructRoadMap(start, goal);
-
-  // Vector2D test(0.2, 0.8);
-  // prm.isFreeSpace(test);
-
-  // prm.printRoadMap();
-
-
   std::vector<planner::Node> nodes;
   prm.getRoadMap(nodes);
 
+  // prm.printRoadMap();
+
+  // marker arrays for prm
   visualization_msgs::MarkerArray nodes_array;
   visualization_msgs::MarkerArray edges_array;
   nodes_array.markers.resize(nodes.size());
   edges_array.markers.resize(nodes.size());
 
-  fillMarkerArray(nodes, nodes_array, edges_array);
-
-   while(node_handle.ok())
-   {
-
-     node_pub.publish(nodes_array);
-     edge_pub.publish(edges_array);
-   }
+  prmMarkerArray(nodes, nodes_array, edges_array, "green");
 
 
+  // find shortest path
+  planner::PRMPlanner plan(prm);
+  plan.planPath();
+
+  // collection of node comprising the path
+  std::vector<Vector2D> path;
+  plan.getPath(path);
+
+  // marker for shortest path
+  visualization_msgs::Marker points, line_strip;
+  pathMarkers(path, points, line_strip);
 
 
+  // for(const auto &elem: path)
+  // {
+  //   std::cout << elem << std::endl;
+  // }
+
+
+  while(node_handle.ok())
+  {
+
+   prm_pub.publish(nodes_array);
+   prm_pub.publish(edges_array);
+
+   path_pub.publish(points);
+   path_pub.publish(line_strip);
+  }
   return 0;
 }
+
+
+
+
+
+void prmMarkerArray(const std::vector<planner::Node> &nodes,
+                     visualization_msgs::MarkerArray &nodes_array,
+                     visualization_msgs::MarkerArray &edges_array,
+                     std::string color)
+{
+
+  for(unsigned int i = 0; i < nodes.size(); i++)
+  {
+
+    // coordinates of node
+    const auto ndx = nodes.at(i).point.x;
+    const auto ndy = nodes.at(i).point.y;
+    geometry_msgs::Point nd_pt;
+    nd_pt.x = ndx;
+    nd_pt.y = ndy;
+    nd_pt.z = 0.0;
+
+
+    // add spheres for nodes
+    nodes_array.markers[i].header.frame_id = frame_id;
+    nodes_array.markers[i].header.stamp = ros::Time::now();
+    nodes_array.markers[i].lifetime = ros::Duration();
+    nodes_array.markers[i].ns = "nodes";
+    nodes_array.markers[i].id = i;
+
+    nodes_array.markers[i].type = visualization_msgs::Marker::SPHERE;
+    nodes_array.markers[i].action = visualization_msgs::Marker::ADD;
+
+    nodes_array.markers[i].pose.position.x = ndx;
+    nodes_array.markers[i].pose.position.y = ndy;
+    nodes_array.markers[i].pose.position.z = 0.0;
+
+    nodes_array.markers[i].pose.orientation.x = 0.0;
+    nodes_array.markers[i].pose.orientation.y = 0.0;
+    nodes_array.markers[i].pose.orientation.z = 0.0;
+    nodes_array.markers[i].pose.orientation.w = 1.0;
+
+    nodes_array.markers[i].scale.x = 0.1;
+    nodes_array.markers[i].scale.y = 0.1;
+    nodes_array.markers[i].scale.z = 0.1;
+
+
+    if (color == "green")
+    {
+      nodes_array.markers[i].color.r = 0.0f;
+      nodes_array.markers[i].color.g = 1.0f;
+      nodes_array.markers[i].color.b = 0.0f;
+      nodes_array.markers[i].color.a = 1.0f;
+    }
+
+    else if (color == "blue")
+    {
+      nodes_array.markers[i].color.r = 0.0f;
+      nodes_array.markers[i].color.g = 0.0f;
+      nodes_array.markers[i].color.b = 1.0f;
+      nodes_array.markers[i].color.a = 1.0f;
+    }
+
+
+    // add line strip for edges
+    edges_array.markers[i].header.frame_id = frame_id;
+    edges_array.markers[i].header.stamp = ros::Time::now();
+    edges_array.markers[i].lifetime = ros::Duration();
+    edges_array.markers[i].ns = "edges";
+    edges_array.markers[i].id = i;
+
+    edges_array.markers[i].type = visualization_msgs::Marker::LINE_LIST;
+    edges_array.markers[i].action = visualization_msgs::Marker::ADD;
+
+    edges_array.markers[i].scale.x = 0.02;
+
+    edges_array.markers[i].pose.orientation.x = 0.0;
+    edges_array.markers[i].pose.orientation.y = 0.0;
+    edges_array.markers[i].pose.orientation.z = 0.0;
+    edges_array.markers[i].pose.orientation.w = 1.0;
+
+    if (color == "green")
+    {
+      edges_array.markers[i].color.r = 0.0f;
+      edges_array.markers[i].color.g = 1.0f;
+      edges_array.markers[i].color.b = 0.0f;
+      edges_array.markers[i].color.a = 1.0f;
+    }
+
+    else if (color == "blue")
+    {
+      edges_array.markers[i].color.r = 0.0f;
+      edges_array.markers[i].color.g = 0.0f;
+      edges_array.markers[i].color.b = 1.0f;
+      edges_array.markers[i].color.a = 1.0f;
+    }
+
+    // loop across adjacency list
+    for(unsigned int j = 0; j < nodes.at(i).edges.size(); j++)
+    {
+      const auto adj_id = nodes.at(i).edges.at(j).id;
+
+      geometry_msgs::Point adj_pt; // adjacent node point
+      adj_pt.x = nodes.at(adj_id).point.x;
+      adj_pt.y = nodes.at(adj_id).point.y;
+      adj_pt.z = 0.0;
+
+      edges_array.markers[i].points.push_back(nd_pt);
+      edges_array.markers[i].points.push_back(adj_pt);
+
+    } // end inner loop
+  } // end outer loop
+}
+
+
+void pathMarkers(const std::vector<Vector2D> &path,
+                 visualization_msgs::Marker &points,
+                 visualization_msgs::Marker &line_strip)
+{
+
+  // points
+  points.header.frame_id = frame_id;
+  points.header.stamp = ros::Time::now();
+  points.lifetime = ros::Duration();
+  points.ns = "path_nodes";
+  points.id = 0;
+
+  points.type = visualization_msgs::Marker::POINTS;
+  points.action = visualization_msgs::Marker::ADD;
+
+  points.pose.orientation.x = 0.0;
+  points.pose.orientation.y = 0.0;
+  points.pose.orientation.z = 0.0;
+  points.pose.orientation.w = 1.0;
+
+  points.scale.x = 0.2;
+  points.scale.y = 0.2;
+
+  points.color.b = 1.0f;
+  points.color.a = 1.0f;
+
+  // line strip
+  line_strip.header.frame_id = frame_id;
+  line_strip.header.stamp = ros::Time::now();
+  line_strip.lifetime = ros::Duration();
+  line_strip.ns = "path_nodes";
+  line_strip.id = 1;
+
+  line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+  line_strip.action = visualization_msgs::Marker::ADD;
+
+  line_strip.pose.orientation.x = 0.0;
+  line_strip.pose.orientation.y = 0.0;
+  line_strip.pose.orientation.z = 0.0;
+  line_strip.pose.orientation.w = 1.0;
+
+  line_strip.scale.x = 0.1;
+
+  line_strip.color.b = 1.0f;
+  line_strip.color.a = 1.0f;
+
+
+  for(unsigned int i = 0; i < path.size(); i++)
+  {
+    geometry_msgs::Point pt;
+    pt.x = path.at(i).x;
+    pt.y =  path.at(i).y;
+    pt.z = 0.0;
+
+    points.points.push_back(pt);
+    line_strip.points.push_back(pt);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
